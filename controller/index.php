fec010485ac467c5bd1fbc1ef05e471d497c8efa
Justin <justin.avril1@etudiant.univ-reims.fr>
2022-06-09 14:49:28 +0200

    loggin method

--- controller/index.php
+++ controller/index.php
@@ -4,3 +4,9 @@ include_once './view/index.php';
 require_once './model/Bdd.php';
 
 $bdd = new Bdd();
+
+if (isset($_POST['submitLoginForm'])) {
+    if (isset($_POST['name']) && isset($_POST['password'])) {
+        $bdd->login($_POST['name'], $_POST['password']);
+    }
+}
